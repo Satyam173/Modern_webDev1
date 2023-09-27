9aeeea8d54f65c1fe7d7c9b59aed20e32eaafbfc
Satyam Mohanty <satyamMohanty112@outlook.com>
2023-09-27 16:26:49 +0530

    1:45

--- script.js
+++ script.js
@@ -3,6 +3,39 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+
+function firstPageAnim() {
+    var tl = gsap.timeline();
+  
+    tl.from("#nav", {
+      y: "-10",
+      opacity: 0,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+    })
+      .to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: 0.2,
+      })
+      .from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+      });
+  } 
+
+// jab mouse move ho toh minicircle jyada distort na ho that means set minimum skew
+function circleChaptaKaro(){
+  window.addEventListener("mousemove",function(dets){
+    
+  })
+}
+
 function circleMouseFollower(){
     window.addEventListener("mousemove",function(dets){
         // console.log(dets);
@@ -10,3 +43,5 @@ function circleMouseFollower(){
     })
 }
 circleMouseFollower();
+
+firstPageAnim();
